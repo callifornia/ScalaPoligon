cf390db7f9d5e6299e68152a5c3bb802f0b9f30a
callifornia <root66929>
2017-11-15 12:41:52 +0200

    Added scaladoc and algorithm runtime

--- src/main/scala/search/BinarySearch.scala
+++ src/main/scala/search/BinarySearch.scala
@@ -6,6 +6,13 @@ import utils.Implicits._
 object BinarySearch {
 
 
+  /**
+    * Binary search takes O(log N) time.
+    *
+    * @param n number which should be found
+    * @param xs array to search in
+    * @return index of n position if element was found, -1 otherwise
+    */
   def findElementInArray(n: Int, xs: Array[Int]): Int = {
 
     def search(n: Int, xs: Array[Int], index: Int): Int = {
